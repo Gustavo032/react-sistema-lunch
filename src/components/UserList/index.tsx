94606c530197c4ae4f3f5f895fc457958d66916b
Gustavo032 <gustavoramosrock@gmail.com>
2024-04-07 05:01:22 -0300

    filtrando fatura do mês

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -7,46 +7,49 @@ const UserList = ({ onSelectUser }:any) => {
   const [filterName, setFilterName] = useState('');
   const [filterId, setFilterId] = useState('');
   const [userTotalPrices, setUserTotalPrices] = useState<any>({});
+	const [selectedMonth, setSelectedMonth] = useState<string>((new Date().getMonth() + 1).toString());
+	const [selectedYear, setSelectedYear] = useState<string>(new Date().getFullYear().toString());
 
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [selectedMonth, selectedYear]);
 
   const fetchUsers = async () => {
-    try {
-      const token = document.cookie.replace(
-        /(?:(?:^|.*;\s*)refreshToken\s*=\s*([^;]*).*$)|^.*$/,
-        '$1'
-      );
-
-      const response = await axios.get('http://localhost:3333/users/all', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const fetchedUsers = response.data.users.user;
-      setUsers(fetchedUsers);
-
-      // Fetch total price sum for each user
-      const userTotalPricePromises = fetchedUsers.map((user:any) => {
-        return axios.get(`http://localhost:3333/total-price-sum/${user.id}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-      });
-
-      const totalPricesResponses = await Promise.all(userTotalPricePromises);
-      const userTotalPricesData = totalPricesResponses.reduce((acc, response, index) => {
-        acc[fetchedUsers[index].id] = response.data.totalPriceSum;
-        return acc;
-      }, {});
-
-      setUserTotalPrices(userTotalPricesData);
-    } catch (error) {
-      console.error('Erro ao buscar usuários:', error);
-    }
-  };
+		try {
+			const token = document.cookie.replace(
+				/(?:(?:^|.*;\s*)refreshToken\s*=\s*([^;]*).*$)|^.*$/,
+				'$1'
+			);
+	
+			const response = await axios.get(`http://localhost:3333/users/all`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const fetchedUsers = response.data.users.user;
+			setUsers(fetchedUsers);
+	
+			// Fetch total price sum for each user
+			const userTotalPricePromises = fetchedUsers.map((user:any) => {
+				return axios.get(`http://localhost:3333/total-price-sum/${user.id}/${String(selectedMonth)}/${String(selectedYear)}`, {
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				});
+			});
+	
+			const totalPricesResponses = await Promise.all(userTotalPricePromises);
+			const userTotalPricesData = totalPricesResponses.reduce((acc, response, index) => {
+				acc[fetchedUsers[index].id] = response.data.totalPriceSum;
+				return acc;
+			}, {});
+	
+			setUserTotalPrices(userTotalPricesData);
+		} catch (error) {
+			console.error('Erro ao buscar usuários:', error);
+		}
+	};
+	
 
   const handleSelectUser = (userId:any) => {
     onSelectUser(userId);
@@ -75,6 +78,26 @@ const UserList = ({ onSelectUser }:any) => {
       <Flex mb="4">
         <Input placeholder="Filtrar por nome" value={filterName} onChange={handleFilterName} mr="2" />
         <Input placeholder="Filtrar por ID" value={filterId} onChange={handleFilterId} />
+				<Input
+					type="number"
+					min="1"
+					max="12"
+					placeholder="Mês"
+					value={selectedMonth}
+					onChange={(e) => {
+						setSelectedMonth(e.target.value);
+						fetchUsers(); // Chama fetchUsers quando o valor do mês é alterado
+					}}
+				/>
+				<Input
+					type="number"
+					placeholder="Ano"
+					value={selectedYear}
+					onChange={(e) => {
+						setSelectedYear(e.target.value);
+						fetchUsers(); // Chama fetchUsers quando o valor do ano é alterado
+					}}
+				/>
       </Flex>
       <Table variant="simple">
         <Thead>
